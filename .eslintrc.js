872172e493dda82f5e08e3136dfd2c6c586a7ba5
Thomas Le Goff <thomas.legoff@azimut.net>
2019-08-14 11:31:50 +0200

    Replace CRLF by LF

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    'extends': 'airbnb-base',
+    'installedESLint': true,
+    'plugins': [
+        'import'
+    ],
+    'rules': {
+        'comma-dangle': "off",
+        'max-len': ["error", 120],
+        "no-restricted-syntax": "off",
+        "newline-per-chained-call": "off",
+        "no-param-reassign": "off",
+        "global-require": "off",
+        "import/no-dynamic-require": "off",
+        "no-console": "off"
+    },
+    'env': {
+        'mocha': true
+    }
+};
